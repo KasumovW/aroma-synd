b4a7eef81dc04fdb901be3e8e8219f0ec8291dcd
Зубайра <69210063+KasumovW@users.noreply.github.com>
2023-05-17 11:42:37 +0300

    Delete src directory

--- src/main.tsx
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import { router } from './routes/root.tsx';
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(router);
-
